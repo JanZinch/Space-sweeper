43d2b296c8bce117c1f0d69f63802925ccaad73a
JanZinch <zinchenko.alexplay@gmail.com>
2022-03-27 19:08:11 +0300

    weapons

--- Assets/Scripts/Entities/Equipment.cs
+++ Assets/Scripts/Entities/Equipment.cs
@@ -1,7 +1,31 @@
 ﻿namespace Entities
 {
-    public class Equipment
+    public class Equipment : ShopItem
     {
+        public EquipmentType Type { get; private set; }
+        public double Power { get; private set; }
+
+        public Equipment(EquipmentType type, double price, double power, ShopItemState state) : base(price, state)
+        {
+            Type = type;
+            Power = power;
+        }
+    }
+
+    public enum EquipmentType
+    {
+        NONE = 0,
+        
+        // weapon
+        
+        MACHINE_GUN = 1,
+        FIREBALL = 2,
+        ROCKETS = 3,
+        LASER = 4
+        
+        // armory
+        
+        
         
     }
 }
--- Assets/Scripts/Utils/PlayerUtils.cs
+++ Assets/Scripts/Utils/PlayerUtils.cs
@@ -1,9 +1,27 @@
-﻿namespace Utils
+﻿using System.Collections.Generic;
+using Entities;
+
+namespace Utils
 {
     public static class PlayerUtils
     {
-        private static int a = 0;
+        private static Dictionary<EquipmentType, Equipment> _weapons = null;
+        public static Equipment CurrentWeapon = null;
 
-        
+
+        public static void Initialize()
+        {
+            _weapons = new Dictionary<EquipmentType, Equipment>()
+            {
+                { EquipmentType.MACHINE_GUN, new Equipment(EquipmentType.MACHINE_GUN, 0.0, 10.0, ShopItemState.EQUIPPED) },
+                { EquipmentType.FIREBALL, new Equipment(EquipmentType.FIREBALL, 0.0, 10.0, ShopItemState.BLOCKED) },
+                { EquipmentType.ROCKETS, new Equipment(EquipmentType.ROCKETS, 0.0, 10.0, ShopItemState.BLOCKED) },
+                { EquipmentType.LASER, new Equipment(EquipmentType.LASER, 0.0, 10.0, ShopItemState.BLOCKED) }
+                
+            };
+
+        }
+
+
     }
 }
