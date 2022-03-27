5020458e82082b2752fffc0bd090dee31244314b
JanZinch <zinchenko.alexplay@gmail.com>
2022-03-27 20:00:40 +0300

    projectiles

---
+++ Assets/Scripts/Entities/DestructibleObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Entities
+{
+    public class DestructibleObject : MonoBehaviour
+    {
+        public double Health { get; private set; } = 100.0f;
+
+        public Func<Tween> OnDeath = null;
+
+        public void MakeDamage(double damage)
+        {
+            Health -= damage;
+
+            if (Health <= 0)
+            {
+                if (OnDeath != null)
+                {
+                    OnDeath().OnComplete(() =>
+                    {
+                        Destroy(this);
+                    });
+                }
+                else Destroy(this);
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Entities/Projectile.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Entities;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody = null;
+    [SerializeField] private ParticleSystem _trailParticles = null;
+    [SerializeField] private ParticleSystem _explosionOriginal = null;
+    
+    
+    private double _damage = 0.0;
+
+    private void Start()
+    {
+        Setup(new Vector3(0.0f, 0.0f, 10.0f), 10.0f);
+    }
+
+    private void Setup(Vector3 velocity, double damage)
+    {
+        _rigidbody.velocity = velocity;
+        _damage = damage;
+        
+        if(_trailParticles) _trailParticles.Play();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.TryGetComponent<DestructibleObject>(out DestructibleObject destructibleObject))
+        {
+            destructibleObject.MakeDamage(_damage);
+            
+            // effects setup
+        }
+    }
+}
