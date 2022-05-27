7a9588addb78a9d73e5c23732130fcb01eeb677a
JanZinch <jan.zinchanka@mail.ru>
2022-05-28 00:51:32 +0300

    fix

---
+++ Assets/Scripts/AimingController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AimingController : MonoBehaviour
+{
+    [SerializeField] private RectTransform _sight = null;
+
+    private const string MouseXAxis = "Mouse X";
+    private const string MouseYAxis = "Mouse Y";
+
+    private Vector2 _cachedMousePosition = default;
+
+    private Vector2 _sourceSightPosition = default;
+
+    private void Start()
+    {
+        _sourceSightPosition = _sight.position;
+    }
+
+    private void Update()
+    {
+        _cachedMousePosition = Input.mousePosition;
+        _sight.position = _cachedMousePosition;
+    }
+
+    private Vector3 GetDirection()
+    {
+        Vector3 sum = _sourceSightPosition + _cachedMousePosition;
+        sum.z = 0.0f;
+        
+        return sum + Vector3.forward;
+    }
+
+
+}
